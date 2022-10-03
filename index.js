f75505d9dfb1bde866cf855d2e42c018e2d92f66
Orenikoe <orental21@gmail.com>
2022-10-03 19:51:42 +0100

    1st

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const importedObj = require('./information')
 
    console.log(cowsay.say({
     text : `I'm a ${importedObj.cowDescObj.name} from ${importedObj.cowDescObj.campus}!`,
-    e : "Oo",
-    T : "U "
+    e : "Oo"
+   
 }));
 
 
